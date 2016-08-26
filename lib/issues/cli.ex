638b5908a504a8d8c4bc3f678416e97756daa393
Palan <pseemangal@hotmail.com>
2016-08-25 21:22:30 -0400

    initial project

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -10,7 +10,11 @@ defmodule Issues.CLI do
     """
 
     def run(argv) do
-        parse_args(argv)
+        IO.puts "Program running"
+
+        argv 
+        |> parse_args
+        |> process
     end
 
     def parse_args(argv) do
@@ -19,9 +23,28 @@ defmodule Issues.CLI do
 
         case parse do
             {[help: true], _, _} -> :help
-            {_, [user, project, count], _} -> {user, project, count}
+            {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
             {_, [user, project], _} -> {user, project, @default_count}
             _ -> :help
         end
+    end
+
+    def process(:help) do
+        IO.puts """
+        usage: issues <user> <project> [ count | #{@default_count}]
+        """
+
+        System.halt(0)
+    end
+
+    def process({user, project, _count}) do
+        Issues.GithubIssues.fetch(user, project)
+        |> decode_response
+        |> convert_to_list_of_maps
+    end
+
+    def sort_into_ascending_order(list_of_issues) do
+        Enum.sort list_of_issues,
+                  fn i1, i2 -> i1["created_at"] <= i2["created_at"]
     end
 end
---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,37 @@
+defmodule Issues.GithubIssues do
+    @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+    @github_url Application.get_env(::issues, :github_url)
+
+    def fetch(user, project) do
+        issues_url(user, project)
+        |> HTTPoison.get(@user_agent)
+        |> handle_response
+    end
+
+    def issues_url(user, project) do
+        "#{github_url}/repos/#{user}/#{project}/issues"
+    end
+
+    def handle_response({:ok, %{status_code: 200, body: body}}) do
+        {:ok, Poison.Parser.parse!(body)}
+    end
+
+    def handle_response({_, %{status_code: _, body: body}}) do
+        {:error, Poison.Parser.parse!(body)}
+    end
+
+    def decode_response({:ok, body}) do
+        body
+    end
+
+    def decode_response({:error, error}) do
+        {_, message} = List.keyfind(error, "message", 0)
+        IO.puts "Error fetching from Github: #{message}"
+        System.halt(2)
+    end
+
+    def convert_to_list_of_maps(list) do
+        list
+        |> Enum.map(&Enum.into(&1, Map.new))
+    end
+end
